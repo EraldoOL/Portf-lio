f423b296b1eed461912e52581f07988870ded6c2
EraldoOL <eo80797@gmail.com>
2024-06-22 15:05:06 -0300

    update

--- Portifólio/script.js
+++ Portifólio/script.js
@@ -14,4 +14,20 @@ const myObserver = new IntersectionObserver((entries) => {
 
 const elements = document.querySelectorAll('.ani')
 
-elements.forEach((element) => myObserver.observe(element))
+elements.forEach((element) => myObserver.observe(element));
+
+
+
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    const mailtoLink = `mailto:eo80797@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}%0D%0A%0D%0ADe:%20${encodeURIComponent(email)}`;
+
+    window.location.href = mailtoLink;
+});
